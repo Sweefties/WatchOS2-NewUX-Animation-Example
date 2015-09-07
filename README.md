fa6af79e9e85326b614a4005b86a74ee53ae2732
Wlad Dicario <wlad@2v-prod.com>
2015-09-07 21:39:45 +0200

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,99 @@
+![](https://img.shields.io/badge/build-pass-brightgreen.svg?style=flat-square)
+![](https://img.shields.io/badge/platform-WatchOS2-ff69b4.svg?style=flat-square)
+![](https://img.shields.io/badge/Require-XCode7-lightgrey.svg?style=flat-square)
+
+
+# WatchOS2 - New UX - Animation - Example
+WatchOS 2 Experiments - New API Components - Animation with duration.
+
+## Example
+
+![](https://raw.githubusercontent.com/Sweefties/WatchOS2-NewUX-Animation-Example/master/source/Apple_Watch_template-Animations.jpg)
+
+
+## Requirements
+
+- >= XCode 7 beta 6~.
+- >= Swift 2.
+
+Tested on WatchOS2 Simulators, Apple Watch.
+
+## Usage
+
+To run the example project, download or clone the repo.
+
+
+### Example Code!
+
+
+Configure :
+
+- Drag and drop more WKInterfaceButton to Interface Controller (Storyboard)
+- connect your WKInterfaceButton to your Interface Controller class
+- define your WKInterfaceButton IBAction
+- put code to your controller class.
+
+
+```swift
+// Interface Group to animate
+@IBOutlet var bubbleGroup: WKInterfaceGroup!
+```
+
+- Properties
+
+```swift
+private let duration = 0.5
+private let alphaTimer = 1.0
+```
+
+- IBAction examples
+
+```swift
+// move bubble to the left
+@IBAction func moveLeftButton() {
+    self.animateWithDuration(duration) { () -> Void in
+        self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Left)
+    }
+}
+// move bubble to the right
+@IBAction func moveRightButton() {
+    self.animateWithDuration(duration) { () -> Void in
+        self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Right)
+    }
+}
+// move bubble to the top
+@IBAction func moveUpButton() {
+    self.animateWithDuration(duration) { () -> Void in
+        self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Top)
+    }
+}
+// move bubble to the bottom
+@IBAction func moveDownButton() {
+    self.animateWithDuration(duration) { () -> Void in
+        self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Bottom)
+    }
+}
+// move bubble to the center (screen)
+@IBAction func moveCenterButton() {
+    self.animateWithDuration(duration) { () -> Void in
+        self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Center)
+        self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Center)
+    }
+}
+// animate bubble with alpha
+@IBAction func changeAlphaButton() {
+    self.animateWithDuration(alphaTimer) { () -> Void in
+        self.bubbleGroup.setAlpha(0)
+    }
+    let completionDelay = dispatch_time(DISPATCH_TIME_NOW, Int64(alphaTimer * Double(NSEC_PER_SEC)))
+    dispatch_after(completionDelay, dispatch_get_main_queue()) {
+        self.animateWithDuration(self.alphaTimer, animations: { () -> Void in
+            self.bubbleGroup.setAlpha(1)
+        })
+    }
+    
+}
+```
+
+
+Build and Run!
--- WatchOS2-NewUX-Animation-Example WatchKit Extension/InterfaceController.swift
+++ WatchOS2-NewUX-Animation-Example WatchKit Extension/InterfaceController.swift
@@ -12,12 +12,33 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    // MARK: - Interface
+    @IBOutlet var bubbleGroup: WKInterfaceGroup!
+    @IBOutlet var leftButton: WKInterfaceButton!
+    @IBOutlet var rightButton: WKInterfaceButton!
+    @IBOutlet var topButton: WKInterfaceButton!
+    @IBOutlet var bottomButton: WKInterfaceButton!
+    @IBOutlet var centerButton: WKInterfaceButton!
+    @IBOutlet var alphaButton: WKInterfaceButton!
+    
+    
+    // MARK: - Properties
+    private let duration = 0.5
+    private let alphaTimer = 1.0
+    
+    
+    // MARK: - Context Initializer
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
         // Configure interface objects here.
+        self.animateWithDuration(duration) { () -> Void in
+            self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Top)
+            self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Center)
+        }
     }
 
+    
+    // MARK: - Calls
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
@@ -27,5 +48,57 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+}
 
+
+//MARK: - Actions -> InterfaceController Extension
+typealias Actions = InterfaceController
+extension Actions {
+    
+    // move bubble to the left
+    @IBAction func moveLeftButton() {
+        self.animateWithDuration(duration) { () -> Void in
+            self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Left)
+        }
+    }
+    // move bubble to the right
+    @IBAction func moveRightButton() {
+        self.animateWithDuration(duration) { () -> Void in
+            self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Right)
+        }
+    }
+    // move bubble to the top
+    @IBAction func moveUpButton() {
+        self.animateWithDuration(duration) { () -> Void in
+            self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Top)
+        }
+    }
+    // move bubble to the bottom
+    @IBAction func moveDownButton() {
+        self.animateWithDuration(duration) { () -> Void in
+            self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Bottom)
+        }
+    }
+    // move bubble to the center (screen)
+    @IBAction func moveCenterButton() {
+        self.animateWithDuration(duration) { () -> Void in
+            self.bubbleGroup.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.Center)
+            self.bubbleGroup.setVerticalAlignment(WKInterfaceObjectVerticalAlignment.Center)
+        }
+    }
+    // animate bubble with alpha
+    @IBAction func changeAlphaButton() {
+        self.animateWithDuration(alphaTimer) { () -> Void in
+            self.bubbleGroup.setAlpha(0)
+        }
+        let completionDelay = dispatch_time(DISPATCH_TIME_NOW, Int64(alphaTimer * Double(NSEC_PER_SEC)))
+        dispatch_after(completionDelay, dispatch_get_main_queue()) {
+            self.animateWithDuration(self.alphaTimer, animations: { () -> Void in
+                self.bubbleGroup.setAlpha(1)
+            })
+        }
+        
+    }
+    
 }
